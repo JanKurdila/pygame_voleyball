24cc3e1f20605f0d4d3ff42ebf1057c782260d1b
JanKurdila <lektorpythonav@gmail.com>
2024-05-21 15:32:37 +0200

    3) Vloženie hráča1  a hráča2

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ import sys
 
 if __name__ == "__main__":
     pygame.init()
-    
+
     window = pygame.display.set_mode(config.ROZLISENIE)
     pygame.display.set_caption("Volejbal")
 
     clock = pygame.time.Clock()
-    
+
+    hrac1 = pygame.Rect(config.ROZLISENIE[0]- 110, config.ROZLISENIE[1]//2 - 50, 10, 100)
+    hrac2 = pygame.Rect(110, config.ROZLISENIE[1]//2 - 50, 10, 100)
+
     while True:
         # Ak vypnem okno, musím vypnuť pygame
         for event in pygame.event.get():
@@ -18,6 +21,9 @@ if __name__ == "__main__":
                 sys.exit() # Vypnutie celého programu
 
         window.fill(config.FARBA_POZADAIA) # Premazanie obrazovky
+
+        pygame.draw.rect(window, "white", hrac1)
+        pygame.draw.rect(window, "white", hrac2)
 
         pygame.display.update()
 
